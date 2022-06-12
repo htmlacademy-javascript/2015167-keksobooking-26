f9e6a70fd4b5b3a1770d6fb0d80c48611d54105a
Евгений <msk9199625687@yandex.ru>
2022-06-12 23:28:15 +0300

    провальная попытка

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-getRandomInt(10, 1);
+getRandomInt(1, 10);
 
 // Задание по кексобукингу
 
@@ -29,4 +29,108 @@ const getRandomFloatInt = (min, max, digits) => {
   return Number((Math.random() * (max - min + 1) + min).toFixed([digits]));
 };
 
-getRandomFloatInt(32, 5303, 5);
+getRandomFloatInt(0, 99, 5);
+
+const idPng = getRandomInt(1, 9);
+const getAvatarValue = function () {
+  if (idPng < 10) {
+    return `img/avatars/user0${idPng}.png`;
+  }
+  return `img/avatars/user${idPng}.png`;
+};
+
+const author = {
+  avatar: getAvatarValue(idPng),
+};
+
+const getLat = getRandomFloatInt(35.65000, 35.70000, 5);
+const getLng = getRandomFloatInt(139.70000, 139.80000, 5);
+const adressValue = `${getLat  }, ${  getLng}`;
+const priceValue = getRandomInt(1, 100000);
+const typeArray = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+
+const roomsValue = getRandomInt(1, 10);
+const guestsValue = getRandomInt(1, 10);
+const checkinArray = ['12:00', '13:00', '14:00'];
+const checkoutArray = ['12:00', '13:00', '14:00'];
+
+// Определяем длину массива, создаём новый массив, перезаписываем новый массив удаляя повторения
+
+const featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const newArrayLength = getRandomInt(0, featuresArray.length - 1);
+const newArray = [];
+const uniqueArray = [];
+const createArray = function () {
+  for (let i = 0; i <= newArrayLength; i++) {
+    const options = getRandomArrayElement(featuresArray);
+    newArray.push(options);
+  }
+
+  newArray.forEach((element) => {
+    if (!uniqueArray.includes(element)) {
+      uniqueArray.push(element);
+    }
+  });
+  return uniqueArray;
+};
+
+// С массивом фото такая же история как и featuresArray только другие названия переменных
+
+const photosArray = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const newPhotoArrayLength = getRandomInt(0, photosArray.length - 1);
+const newPhotoArray = [];
+const uniquePhotoArray = [];
+const createPhotoArray = () => {
+  for (let i = 0; i <= newPhotoArrayLength; i++) {
+    const options = getRandomArrayElement(photosArray);
+    newPhotoArray.push(options);
+  }
+
+  newPhotoArray.forEach((element) => {
+    if (!uniquePhotoArray.includes(element)) {
+      uniquePhotoArray.push(element);
+    }
+  });
+  return uniquePhotoArray;
+};
+
+const offer = {
+  title: 'Лучшее предложение по Вашим критериям',
+  adress: adressValue,
+  price: priceValue,
+  type: getRandomArrayElement(typeArray),
+  rooms: roomsValue,
+  guests: guestsValue,
+  checkin: getRandomArrayElement(checkinArray),
+  checkout: getRandomArrayElement(checkoutArray),
+  features: createArray(featuresArray),
+  description: 'Шведский стол на завтрак включен в стоимость проживания',
+  photos: createPhotoArray(photosArray),
+};
+
+//Изменил location на название myLocation потому что в консоле выдавало ошибку, что слово уже используется. Я так понимаю глобальное название
+
+const myLocation = {
+  lat: getLat,
+  lng: getLng,
+};
+
+const createObject = () => ({
+  author,
+  offer,
+  myLocation,
+});
+
+const requiredQuantity = 10;
+
+const createRequiredQuantityObjects = Array.from({length:requiredQuantity}, createObject);
+
+createObject();
+createRequiredQuantityObjects();
