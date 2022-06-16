766d3a2fca3dd0d1a894a8ed7903b4fef544a3fe
Евгений <msk9199625687@yandex.ru>
2022-06-16 23:39:27 +0300

    fix name const

--- js/main.js
+++ js/main.js
@@ -49,26 +49,26 @@ const getLng = () => getRandomFloatInt(139.70000, 139.80000, 5);
 const adressValue = () => `${getLat()}, ${getLng()}`;
 const getPriceValue = () => getRandomInt(1, 100000);
 
-const typeArray = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
 const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
 
 const getRoomsValue = () => getRandomInt(1, 10);
 const getGuestsValue = () => getRandomInt(1, 10);
 
-const checkinArray = ['12:00', '13:00', '14:00'];
-const checkoutArray = ['12:00', '13:00', '14:00'];
+const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 
 // Определяем длину массива, создаём новый массив, перезаписываем новый массив удаляя повторения
 
-const featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const newArrayLength = () => getRandomInt(0, featuresArray.length - 1);
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const newArrayLength = () => getRandomInt(0, FEATURES.length - 1);
 
 const createArray = () => {
   const newArray = [];
   const uniqueArray = [];
   for (let i = 0; i <= newArrayLength(); i++) {
-    const options = getRandomArrayElement(featuresArray);
+    const options = getRandomArrayElement(FEATURES);
     newArray.push(options);
   }
 
@@ -82,17 +82,17 @@ const createArray = () => {
 
 // С массивом фото такая же история как и featuresArray только другие названия переменных
 
-const photosArray = [
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
-const newPhotoArrayLength = () => getRandomInt(0, photosArray.length - 1);
+const newPhotoArrayLength = () => getRandomInt(0, PHOTOS.length - 1);
 
 const createPhotoArray = () => {
   const newPhotoArray = [];
   const uniquePhotoArray = [];
   for (let i = 0; i <= newPhotoArrayLength(); i++) {
-    const options = getRandomArrayElement(photosArray);
+    const options = getRandomArrayElement(PHOTOS);
     newPhotoArray.push(options);
   }
 
@@ -108,14 +108,14 @@ const getOffer = () => ({
   title: 'Лучшее предложение по Вашим критериям',
   adress: adressValue(),
   price: getPriceValue(),
-  type: getRandomArrayElement(typeArray),
+  type: getRandomArrayElement(TYPES),
   rooms: getRoomsValue(),
   guests: getGuestsValue(),
-  checkin: getRandomArrayElement(checkinArray),
-  checkout: getRandomArrayElement(checkoutArray),
-  features: createArray(featuresArray),
+  checkin: getRandomArrayElement(CHECKIN_TIMES),
+  checkout: getRandomArrayElement(CHECKOUT_TIMES),
+  features: createArray(FEATURES),
   description: 'Шведский стол на завтрак включен в стоимость проживания',
-  photos: createPhotoArray(photosArray),
+  photos: createPhotoArray(PHOTOS),
 });
 
 //Изменил location на название myLocation потому что в консоле выдавало ошибку, что слово уже используется. Я так понимаю глобальное название
