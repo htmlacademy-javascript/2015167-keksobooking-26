b7420a688f0c220a70124f452fed8bc47bdb76ae
Евгений <msk9199625687@yandex.ru>
2022-06-30 16:14:06 +0300

    slider dead

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,23 @@
-const slider = document.getElementById('slider');
-
-noUiSlider.create(slider, {
-  start: [20, 80],
-  connect: true,
+const rangeSlider = document.getElementById('slider-range');
+const inputValue = document.querySelector('.ad-form__value-price');
+noUiSlider.create(rangeSlider, {
+  start: [5000],
   range: {
-    'min': 0,
-    'max': 100
+    'min': [0],
+    'max': [100000]
   }
 });
+
+// console.log(typeof(toggleRange))
+// console.log(typeof(inputValue.value))
+inputValue.addEventListener('input', () =>{
+  // let toggleRange = rangeSlider.querySelector('.noUi-handle').ariaValueNow;
+  rangeSlider.querySelector('.noUi-handle').ariaValueText = inputValue.value ;
+});
+
+
+// const rangeSliderValueElement = document.getElementById('.ad-form__value-price');
+
+// rangeSlider.noUiSlider.on('update', (values, handle) => {
+//   rangeSliderValueElement.innerHTML = values[handle];
+// });
