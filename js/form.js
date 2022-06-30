d48ae327662773ab97547c86fe794645821e46c8
Евгений <msk9199625687@yandex.ru>
2022-06-30 14:27:39 +0300

    validation

--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 const formPublic = document.querySelector ('.ad-form');
 const formFind = document.querySelector ('.map__filters');
 
+
 const doFormPublicDisabled = () => {
   formPublic.classList.add('ad-form--disabled');
   formPublic.childNodes.disabled = true;
@@ -49,4 +50,46 @@ const doFormsEnabled = () =>{
 
 doFormsEnabled();
 
+const onButtonSubmit = formPublic.querySelector('.ad-form__submit');
+const pristine = new Pristine(formPublic, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__error-text'
+});
+const roomNumber = formPublic.querySelector('#room_number');
+const guestNumber = formPublic.querySelector('#capacity');
 
+pristine.addValidator(roomNumber, () => {
+  if (roomNumber.value === '1' && guestNumber.value === '1'){
+    return true;
+  }
+  if (roomNumber.value === '2' && guestNumber.value === '2'){
+    return true;
+  }
+  if (roomNumber.value === '2' && guestNumber.value === '1'){
+    return true;
+  }
+  if (roomNumber.value === '3' && guestNumber.value === '1' ){
+    return true;
+  }
+  if (roomNumber.value === '3' && guestNumber.value === '2' ){
+    return true;
+  }
+  if (roomNumber.value === '3' && guestNumber.value === '3' ){
+    return true;
+  }
+  if (roomNumber.value === '100' && guestNumber.value === '0' ){
+    return true;
+  }
+  return false;
+}, 'Измените количество комнат', 4, false);
+
+
+formPublic.addEventListener('submit', (evt)=>{
+  const isValid = pristine.validate();
+  if (!isValid) {
+    evt.preventDefault();
+  } else {
+    onButtonSubmit.disabled = true;
+  }
+});
